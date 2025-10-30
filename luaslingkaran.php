c7f1b4224c4aada04b5bd72fb99654c9c265bffa
angel9706 <sherenangelinwijaya89@gmail.com>
2025-10-30 13:02:22 +0700

    pertemuan5

--- luaslingkaran.php
+++ luaslingkaran.php
@@ -3,9 +3,26 @@ class LuasLingkaran{
     public const PHI = 3.14;
     public int $Jari;
 
+    public function __construct($isiJari = 1) {
+        $this->Jari = $isiJari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $Luas = LuasLingkaran::PHI * $this->Jari * $this->Jari;
+        echo "Luas Lingkaran dengan jari-jari $nama cm adalah $Luas cm2";
+    }
+
+    public static function testing(){
+        echo"<br>";
+        echo"ini testing static";
+    }
+
+    public function __destruct() {
+        echo "<br>saya lelah";
+    }
 }
-$Lingkaran = new LuasLingkaran();
-$Lingkaran->Jari = 10;
-$Luas = LuasLingkaran::PHI * $Lingkaran->Jari * $Lingkaran->Jari;
-echo "Luas Lingkaran dengan jari-jari $Lingkaran->Jari cm adalah $Luas cm2";
-?>
+$Lingkaran = new LuasLingkaran(10);
+$Lingkaran->tampil('roda'); //panggil method tampil
+
+LuasLingkaran::testing(); //panggil method static
+
